a605d9da6c10eaab1bbedca8155dd9a693c4a046
Grzegorz <jifwin@users.noreply.github.com>
2025-05-04 08:05:00 +0200

    cleanup

--- amplify/functions/dynamoDB-function/resource.ts
+++ amplify/functions/dynamoDB-function/resource.ts
@@ -1,13 +1,6 @@
 import { defineFunction } from "@aws-amplify/backend";
-import { defineTable } from '@aws-amplify/backend';
-
-export const myTable = defineTable({
-    partitionKey: 'id',
-    name: 'MyDynamoDBTable',
-});
 
 export const myDynamoDBFunction = defineFunction({
     name: "dynamoDB-function",
     resourceGroupName: "data",
-    permissions: [myTable.permissions.write()],
 });
